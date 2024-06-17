305057051c038664d908d1d8b8b1e0c64d493a3d
Tomás <arnotomas1@gmail.com>
2024-06-17 20:30:53 -0300

    Deploy working. (#25)
    * branches removed
    
    * problems solved
    
    * problems merge solved
    
    * problems with dependency
    
    * command start fixed
    
    * testing problems with clusterization

--- back/src/index.js
+++ back/src/index.js
@@ -3,8 +3,8 @@ import createCluster from "./utils/createCluster.utils.js";
 import Logger from "./utils/winston.utils.js";
 
 const PORT = process.env.PORT || 8080;
-const cbReady = () => Logger.info('Server running on port ' + PORT)
+const cbReady = () => console.log('Server running on port ' + PORT)
 
-createCluster(() => app.listen(PORT, cbReady))
+app.listen(PORT, cbReady)
 
 
