08fdb7cfafa33ba1b0910fe9a2b90df77c9dc17e
Tomás <arnotomas1@gmail.com>
2024-06-18 00:05:44 -0300

    Update constants.js

--- front/src/utils/constants.js
+++ front/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const apiUrl = `http://localhost:54000/api`;
-// export const apiUrl = `https://node-55655-production.up.railway.app/api`;
+// export const apiUrl = `http://localhost:54000/api`;
+export const apiUrl = `https://node-55655-production.up.railway.app/api`;
