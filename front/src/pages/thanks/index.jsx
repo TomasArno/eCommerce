fb2efd1ed55442b7f8eaa7e209d40821da8a8301
Tomás <arnotomas1@gmail.com>
2024-06-18 00:41:25 -0300

    style added (#27)
    * branches removed
    
    * problems solved
    
    * problems merge solved
    
    * problems with dependency
    
    * command start fixed
    
    * testing problems with clusterization
    
    * sign up fixed
    
    * register fixed
    
    * styles fixed

--- front/src/pages/thanks/index.jsx
+++ front/src/pages/thanks/index.jsx
@@ -2,22 +2,28 @@ import { useNavigate } from "react-router-dom";
 
 import Box from "@mui/joy/Box";
 import Stack from "@mui/joy/Stack";
+import Typography from '@mui/joy/Typography';
 
 function Thanks() {
   const navigate = useNavigate();
 
+  setTimeout(() => navigate("/"), 3000)
+  
   return (
     <Box id={123} sx={{ flex: 1, width: "100%", background: "#ddd" }}>
       <Stack
         spacing={4}
         sx={{
           display: "flex",
+          alignItems: "center",
           maxWidth: "800px",
           mx: "auto",
           px: { xs: 2, md: 6 },
           py: { xs: 2, md: 10 },
         }}
       >
+        <Typography level="h1">¡MUCHAS GRACIAS POR SU COMPRA!</Typography>
+        <Typography level="h3">Será redirido a la página principal</Typography>
       </Stack >
     </Box >
   );
